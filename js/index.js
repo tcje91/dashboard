71fee18f09b1a0510afbeb97f352636683204b34
tcje91 <tcje9117@outlook.com>
2019-05-06 15:28:58 +0100

    ensure table view only useable on larger media

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 function handleLayoutChange() {
   const main = document.getElementById('main');
-  const value = document.querySelector('input[name="layout"]:checked').value;
-  if (value === 'table') {
+  const layout = document.querySelector('input[name="layout"]:checked').value;
+  if (layout === 'table') {
     main.classList.add('table');
-  } else if (value === 'cards') {
+  } else if (layout === 'cards') {
     main.classList.remove('table');
   }
 }
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    const main = document.getElementById('main');
+    main.classList.remove('table');
+    document.getElementById('cards').checked = true;
+  }
+});
